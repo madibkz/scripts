d5f12e9f59a7d7a8d62717b56cf29aa503c222ea
Madi Baig <madibaig18@yahoo.com>
2021-03-24 17:26:49 +0000

    Changed slow down audio folder script

--- slow_down_audio_folder.sh
+++ slow_down_audio_folder.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 slowdownrate="70"
+suffix="-slowed.mp3"
 
 if [ -n "${1+set}" ]
 then
   $slowdownrate=$1
 fi
 
+detox ./ #remove spaces / bad characters in file names
+
 for f in $(ls);
 do
-  ecasound -i "$f" -ei:$slowdownrate -o "$f" ;
+  ffmpeg -i "$f" "$f$suffix" ; #use ffmpeg to convert the file to mp3
+  ecasound -i "$f$suffix" -ei:$slowdownrate -o "$f$suffix" ; # replace with slowed file
 done ;
